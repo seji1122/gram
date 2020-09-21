2522d6a53e847cefaba89a6707285742934479da
seji1122 <68591263+seji1122@users.noreply.github.com>
2020-09-21 15:17:05 +0900

    投稿機能実装

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,27 @@
+class PostsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to post_path(@post)
+  end
+
+  def edit
+  end
+
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :image)
+  end
+end
